0c18832958f5078301771e4fe06f53438926f6d0
Bonfacegitonga <bonfaceg303@gmail.com>
2022-08-21 15:29:23 +0300

    Initial commit

---
+++ BulkyBook/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BulkyBook.Data;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objectCategoryList = _db.Categories;
+            return View(objectCategoryList);
+        }
+    }
+}
